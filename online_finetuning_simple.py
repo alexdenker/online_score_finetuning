d5ff922b1db4f6ee935b2d5fe97f2354be7e1d46
adenker <adenker@uni-bremen.de>
2024-01-15 11:12:05 +0100

    initial experiments

--- online_finetuning_simple.py
+++ online_finetuning_simple.py
@@ -1,16 +1,17 @@
 
 
+"""
 
+Only try to learn a simple neural network giving me the step length per time step: 
 
+s_theta(x_t, t) - NN(t) A*(A x_t - y)
 
-
+Full backpropagation through the solver
+"""
 
 
 import torch
 import torchsde
-
-
-
 
 import numpy as np 
 import matplotlib.pyplot as plt 
@@ -18,6 +19,7 @@ import os
 import yaml 
 
 import torch 
+import torch.nn as nn 
 
 from torchvision.datasets import MNIST
 from torchvision import transforms 
@@ -29,9 +31,7 @@ from src import TinyUnet, VPSDE, BaseSampler, Euler_Maruyama_sde_predictor
 
 base_path = "model_weights"
 
-
 with open(os.path.join(base_path, "report.yaml"), "r") as f:
-    cfg_dict = yaml.safe_load(f)
     cfg_dict = yaml.safe_load(f)
 
 sde = VPSDE(beta_min=cfg_dict["diffusion"]["beta_min"], 
@@ -59,7 +59,7 @@ val_dataset = MNIST(root="./mnist_data",
 
 x_gt = val_dataset[0][0].unsqueeze(0).to("cuda")
 
-m = x_gt.numel()/4
+m = x_gt.numel()/2
 A = torch.randn([int(m), int(x_gt.numel())])/torch.sqrt(torch.tensor(m))
 A = A.to("cuda")
 
@@ -102,16 +102,14 @@ class SDE(torch.nn.Module):
 
         self.model = model 
         self.sde = sde 
-        self.new_model = TinyUnet(
-            marginal_prob_std=sde.marginal_prob_std, 
-            time_embedding_dim=cfg_dict["model"]["time_embedding_dim"],
-            max_period=cfg_dict["model"]["max_period"],
-            in_channels=2,
-            out_channels=cfg_dict["model"]["out_channels"],
-            base_dim=16,
-            dim_mults=[1,2])
-        self.new_model.to("cuda")
-        self.new_model.train() 
+        self.time_model = nn.Sequential(nn.Linear(1, 32),
+                                    nn.ReLU(), 
+                                    nn.Linear(32,1))
+        for p in self.time_model[-1].parameters():
+            p.detach().zero_()
+
+        self.time_model.to("cuda")
+        self.time_model.train() 
 
         self.cond = cond
     # Drift
@@ -122,19 +120,20 @@ class SDE(torch.nn.Module):
         ones_vec = torch.ones(y.shape[0], device=y.device)
         t = ones_vec * t
         
-        model_inp = torch.cat([y.view(y.shape[0], 1, 28,28), self.cond], dim=1)
         s_pretrained = self.model(y.view(y.shape[0], 1, 28,28), 1.0 - t)
-        s_new = self.new_model(model_inp, 1.0 - t)
+        
+        time_cond = self.time_model(1.0 - t.unsqueeze(1))
+        s_new = - time_cond[:,None,None]*Abwd(Afwd(y.view(y.shape[0], 1, 28,28)) - self.cond)/torch.max(self.model.marginal_prob_std(1.0 - t)[:, None, None, None], torch.tensor(0.05,device=y.device))
+
         s = s_pretrained + s_new
 
         drift, diffusion = self.sde.sde(y.view(y.shape[0], 1, 28,28), 1.0 - t)
 
         mu = drift - diffusion[:, None, None, None].pow(2)*s
 
-        f_sq = (s_new.view(y.shape[0], -1) ** 2).sum(dim=1, keepdim=True)
-
+        f_sq = (time_cond ** 2).sum(dim=1, keepdim=True)
         drift = -mu.view(y.shape[0], -1)
-
+        
         return torch.cat([drift, f_sq], dim=1)
 
     # Diffusion
@@ -147,14 +146,14 @@ class SDE(torch.nn.Module):
         diffusion_rep = diffusion[:,None].repeat(1, y.shape[-1])
         return torch.cat([diffusion_rep, torch.zeros((y.shape[0], 1), device=y.device)], dim=1)
 
-batch_size = 128
+batch_size = 4
 
-cond = torch.repeat_interleave(ATy,  dim=0, repeats=batch_size)
+cond = torch.repeat_interleave(y_noise,  dim=0, repeats=batch_size)
 
 sde_model = SDE(model=model, sde=sde, cond=cond)
-t_size = 800
+t_size = 80
 
-optimizer = torch.optim.Adam(sde_model.new_model.parameters(), lr=1e-3)
+optimizer = torch.optim.Adam(sde_model.time_model.parameters(), lr=1e-3)
 
 x_target = x_gt.repeat(batch_size, 1, 1, 1)
 
@@ -165,38 +164,43 @@ for i in range(1000):
 
     y0 = torch.randn((batch_size, 784)).to("cuda")
     y0 = torch.cat([y0, torch.zeros((batch_size, 1), device=y0.device)], dim=1)
-    print(y0.shape)
-    bm = torchsde.BrownianInterval(t0=0.0, t1=1.0 - 1.e-5, size=(batch_size, 784 + 1), device='cuda')
+    bm = torchsde.BrownianInterval(t0=0.0, t1=1.0 - 1.e-3, size=(batch_size, 784 + 1), device='cuda')
 
-    ts = torch.linspace(0, 1 - 1.e-5, t_size).to("cuda")
+    ts = torch.linspace(0, 1 - 1.e-3, t_size).to("cuda")
 
     #ys, logpq = torchsde.sdeint_adjoint(sde_model, y0, ts, method='euler', logqp=True)
     #ys = torchsde.sdeint_adjoint(sde_model, y0, ts, method='euler',adjoint_method="euler", dt=0.01)
-    ys = torchsde.sdeint_adjoint(sde_model, y0, ts, method='euler',adjoint_method="euler", dt=ts[1] - ts[0], bm=bm)
+    ys = torchsde.sdeint(sde_model, y0, ts, method='euler', dt=ts[1] - ts[0], bm=bm)
 
-    print("output shape ", ys.shape)
     #print(logpq.shape)
     ys_img = ys[-1, :, :-1]
-    print("img shape? ", ys_img.shape)
     ys_img = ys_img.view(batch_size, 1, 28, 28)
 
     f_sq = ys[-1, :, -1]
-    print(f_sq)
     #loss = torch.sum(logpq**2) + 1/2*torch.sum((Afwd(ys) - y_noise)**2)
     loss_data = 1/2 * torch.mean(torch.sum((ys_img - x_target)**2, dim=(1,2,3)))
+    print(loss_data, torch.mean(f_sq))
     loss = loss_data + torch.mean(f_sq) #1/2*torch.mean((Afwd(ys) - y_noise)**2)
     print(loss.item())
     loss.backward()
 
     optimizer.step()
+    
+    img_grid = make_grid(ys_img.cpu(), n_row=4)
 
-    if i % 50 == 0 and i > 0:
-        sde_model.new_model.eval()
+    fig, (ax1, ax2) = plt.subplots(1,2)
+    ax1.imshow(x_target[0,0,:,:].cpu().numpy(), cmap="gray")
+    ax2.imshow(img_grid[0,:,:].numpy(), cmap="gray")
+    plt.savefig(f"test_imgs/iter_{i}.png")
+    plt.close()
+
+    """
+    if i % 5 == 0:# and i > 0:
         with torch.no_grad():
             y0 = torch.randn((batch_size, 784)).to("cuda")
             y0 = torch.cat([y0, torch.zeros((batch_size, 1), device=y0.device)], dim=1)
 
-            ts = torch.linspace(0, 1 - 1.e-5, t_size).to("cuda")
+            ts = torch.linspace(0, 1 - 1.e-3, t_size).to("cuda")
             #ys, logpq = torchsde.sdeint_adjoint(sde_model, y0, ts, method='euler', logqp=True)
             #ys = torchsde.sdeint_adjoint(sde_model, y0, ts, method='euler',adjoint_method="euler", dt=0.01)
             ys = torchsde.sdeint(sde_model, y0, ts, method='euler',dt=ts[1] - ts[0], bm=bm)
@@ -211,4 +215,4 @@ for i in range(1000):
         plt.imshow(img_grid[0,:,:].numpy(), cmap="gray")
         plt.show()
 
-        sde_model.new_model.train()
+    """
